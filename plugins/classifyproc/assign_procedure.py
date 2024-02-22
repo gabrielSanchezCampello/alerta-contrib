40b2bd053c42ec0c8d914c74b8e1105fdc0d0d63
Gabriel <gabriel.sanchez.campello@gmail.com>
2024-02-22 09:58:28 +0100

    mejora de logs

--- plugins/classifyproc/assign_procedure.py
+++ plugins/classifyproc/assign_procedure.py
@@ -13,13 +13,15 @@ class AssignProcedure(PluginBase):
         rules_path = "/app/proc_rules.txt"
         event = alert.event
         with open(rules_path, "r") as f:
+            LOG.info("Se lee el fichero")
             for rule in f.readlines():
                 data_rule = rule.split(";")
                 error = data_rule[0]
                 proc = data_rule[1]
                 extra = data_rule[2]
-
+                LOG.info(f"Se buscan coincidencias... {event}=={error}")
                 if event == error:
+                    LOG.info("Coincide")
                     alert.value = proc
                     break
                 if extra:
